943b4405e4128637381ca3c695e27d72f7dd680a
bella <bella@localhost>
2024-02-27 14:30:14 -0800

    Entry from Isabella Spradling - test 1

---
+++ Lab02/src/Hello/Hello.java
@@ -0,0 +1,21 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("217952541 - This line was added by Isabella Spradling.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
+
+}
