5149894c7231ae3055ebaa37f11d87e843fac0ab
snowt <snowt@LAPTOP-O5GBHCP6>
2024-02-28 12:15:10 -0800

    Added Paul's ID number

--- Lab02/src/Main/Hello.java
+++ Lab02/src/Main/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Paul Felker.");
+		System.out.println("303881786 - This line was added by Paul Felker.");
 		System.out.println("217952541 - This line was added by Isabella Spradling.");
 		System.out.println("303744532 - This line was added by Justin Mariano.");
 
